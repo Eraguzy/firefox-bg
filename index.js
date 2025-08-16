4c9a079cb1906eb2878150fdfd6d2a419347d1d2
Eraguzy <chekhab.elias@gmail.com>
2025-08-16 15:04:50 +0200

    add : completed search bar

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const searchButton = document.getElementById('searchButton');
 
 const ul = document.getElementById('suggestions');
 let suggestions = [];
+let searchIndex = -1;
 
 const form = document.querySelector('form');
 
@@ -31,6 +32,7 @@ searchInput.addEventListener('input', (e) => {
 			ul.innerHTML = '';
 			for (let i = 0; i < suggestions.length; i++) {
 				const li = document.createElement('li');
+				li.id = `suggestion-${i}`; // unique ID for each suggestion
 				const divFromLi = document.createElement('div');
 
 				divFromLi.addEventListener('mousedown', () => {
@@ -52,22 +54,36 @@ searchInput.addEventListener('input', (e) => {
 searchInput.addEventListener('focus', () => {
 	document.getElementById('suggestions').style.display = 'block';
 
-	// add keyboard navigation
-	searchInput.addEventListener('keydown', (e) => {
-		index = -1;
-		if (e.key == 'ArrowDown') {
-		}
-		if (e.key == 'ArrowUp') {
-		}
-		if (e.key == 'Enter') {
-		}
-	});
-
 	if (!Array.isArray(suggestions) || suggestions.length === 0) return;
 	searchWrapper.classList.add('withSuggestions');
 });
 
-searchInput.addEventListener('blur', () => { // focus lost
+// handle scrolling through suggestions
+searchInput.addEventListener('keydown', (e) => {
+	let previous = document.getElementById(`suggestion-${searchIndex}`)
+
+	if (['ArrowDown', 'ArrowUp'].includes(e.key)) {
+		e.preventDefault(); // prevent cursor going to the beginning/end of input
+		previous ? previous.classList.remove("selected") : null;
+
+		if (e.key == 'ArrowDown') {
+			searchIndex < suggestions.length - 1 ? searchIndex++ : searchIndex = 0; // iterate and loop around
+		} else if (e.key == 'ArrowUp') {
+			searchIndex > 0 ? searchIndex-- : searchIndex = suggestions.length - 1;
+		}
+		let next = document.getElementById(`suggestion-${searchIndex}`)
+		if (next) document.getElementById(`suggestion-${searchIndex}`).classList.add("selected");
+
+		searchInput.value = next.textContent;
+	}
+
+	if (searchInput.value.trim() === "") { // reset index if input is empty
+		searchIndex = -1;
+	}
+});
+
+// focus lost
+searchInput.addEventListener('blur', () => {
 	document.getElementById('suggestions').style.display = 'none';
 	searchWrapper.classList.remove('withSuggestions');
 });
